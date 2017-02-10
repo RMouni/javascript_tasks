7f8c0579aa6ed18070c7bb75cc1ee3eef07050ea
RMouni <rimmalapudimounika@gmail.com>
2017-02-10 14:08:32 +0530

    task 4 completed

---
+++ Main.java
@@ -0,0 +1,11 @@
+/*<APPLET CODE = "Main.class" Width = "800" HEIGHT = "500" ></APPLET>*/
+import java.applet.*;
+import java.awt.*;
+
+public class Main extends Applet
+{
+	public void paint(Graphics g)
+	{
+		g.drawString("Welcome in Java Applet.",40,20);
+	}
+}
